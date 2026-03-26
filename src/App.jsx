aafd0d19a2008fc94903c194b23a0c17c215a661
SHShadhin <shshadhin0@gmail.com>
2026-03-26 18:40:51 +0600

    create fake player data in jason format and fetch data in react components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import { Suspense } from 'react';
 import './App.css'
 import Banner from './components/banner/Banner';
 import Navbar from './components/navbar/Navbar';
+import Players from './components/players/Players';
 
+const fetchPlayer = async () => {
+  const res = await fetch("/data.json");
+  return res.json();
+}
 function App() {
-
+  const playerPromise = fetchPlayer();
   return (
     <>
       <Navbar></Navbar>
       <Banner></Banner>
+      <Suspense
+        fallback={<span className="loading loading-dots loading-xl"></span>}
+      >
+        <Players playerPromise={playerPromise}>
+          {playerPromise.length}
+        </Players>
+      </Suspense>
     </>
   );
 }
